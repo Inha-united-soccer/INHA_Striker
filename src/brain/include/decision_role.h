0525d7f83882b7fa698f21bfbfc9e93af0ae7d89
alsgyu <alsgyu@users.noreply.github.com>
2026-01-01 20:34:15 +0900

    fix: add

--- src/brain/include/decision_role.h
+++ src/brain/include/decision_role.h
@@ -19,6 +19,7 @@ public:
             InputPort<double>("chase_threshold", 1.0, "이 거리보다 멀어지면 공을 추격하는 동작을 수행"),
             InputPort<string>("decision_in", "", "이전 틱에서의 decision 값을 읽기 위해 사용"),
             InputPort<string>("position", "offense", "offense | defense, 공을 어느 방향으로 찰지 결정"),
+            InputPort<double>("kick_y_offset", 0.0, "킥 시 공을 로봇 중심에서 y축으로 얼마나 오프셋 시킬지 결정"),
             OutputPort<string>("decision_out")};
     }
 
